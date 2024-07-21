64855d9f310de271f7ffdd04f977aaaf59cf18ff
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-07-21 16:18:56 -0300

    ATT: add fn para compilar codigo C

--- lua/custom/funcoes.lua
+++ lua/custom/funcoes.lua
@@ -26,12 +26,28 @@ end
 
 vim.cmd 'command! -nargs=0 RunCode lua RunCode()'
 
---
+function CompileCode()
+  local current_file = vim.fn.expand '%'
+  local ext = vim.fn.fnamemodify(current_file, ':e')
+  if ext == 'c' then
+    local output_file = vim.fn.expand '%:r'
+    vim.cmd 'w' -- Save the current buffer
+    vim.cmd('!' .. 'gcc -o ' .. output_file .. ' ' .. current_file) -- Compile with gcc
+  else
+    vim.cmd 'w'
+  end
+end
+
+vim.cmd 'command! -nargs=0 CompileCode lua CompileCode()'
+
 -- Para salvar usando o Black
 vim.keymap.set('n', '<leader>cw', ':SaveCode<CR>', { desc = '[C]ode [S]ave' })
 
--- Para salvar usando o Black e rodar
+-- Para rodar o código
 vim.keymap.set('n', '<leader>cr', ':RunCode<CR>', { desc = '[C]ode [R]un' })
+
+-- Para compilar
+vim.keymap.set('n', '<leader>cc', ':CompileCode<CR>', { desc = '[C]ode [C]ompile' })
 
 -- Para o obsidian-neovim
 vim.opt.conceallevel = 1
