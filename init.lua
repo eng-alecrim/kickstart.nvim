fa9c5879e8ad1d6823b78a2b5c042cab3235b014
Ihsan Tonuzi <115842560+iton0@users.noreply.github.com>
2024-08-25 23:28:26 -0400

    fix: remove deprecated opt for conform.nvim (#1070)
    - changed lsp_fallback -> lsp_format
    - updated format_on_save function to reflect change above

--- init.lua
+++ init.lua
@@ -887,16 +887,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-<<<<<<< HEAD
       vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'catppuccin-mocha'
-=======
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
->>>>>>> 8944c99 (ATT: formata arquivos .c, numeros relativos, colorscheme tokyo)
     end,
   },
 
